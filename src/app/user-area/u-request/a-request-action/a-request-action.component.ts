4f761f49732361ff3fb31eb3f2cf87f5d42393c8
Sailee Akim <saileeakim@gmail.com>
2022-05-27 12:01:06 +0530

    file upload solved

--- src/app/user-area/u-request/a-request-action/a-request-action.component.ts
+++ src/app/user-area/u-request/a-request-action/a-request-action.component.ts
@@ -1,93 +1,102 @@
-import { COMMA, ENTER } from "@angular/cdk/keycodes";
-import { Component, Inject } from "@angular/core";
-import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
-import { MatChipInputEvent } from "@angular/material/chips";
-
-import { MatDialogModule, MatDialogRef, MatDialogTitle } from '@angular/material/dialog';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { Component, Inject } from '@angular/core';
 import {
-    MAT_DIALOG_DATA
-} from "@angular/material/dialog";
-import { BackendUrlComponent } from "src/app/common/backend-url";
-import { LoadUserDataComponent } from "src/app/common/load-user-data";
-import { SharedVariablesComponent } from "src/app/common/shared-variables";
-import { UserService } from "src/app/_services/user.service";
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { MatChipInputEvent } from '@angular/material/chips';
+import { HttpClient } from '@angular/common/http';
+
+import {
+  MatDialogModule,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { BackendUrlComponent } from 'src/app/common/backend-url';
+import { LoadUserDataComponent } from 'src/app/common/load-user-data';
+import { SharedVariablesComponent } from 'src/app/common/shared-variables';
+import { UserService } from 'src/app/_services/user.service';
 
 @Component({
-    selector: "dialog--request-b",
-    templateUrl: 'a-request-action.component.html'
+  selector: 'dialog--request-b',
+  templateUrl: 'a-request-action.component.html',
 })
 export class ARequestActionComponent {
-    actionType: any;
-    dialogData: any;
-    actionsDataColumns: any;
-    actionsData: any;
-    createRequestForm: any;
-    file: File;
-    userInfo : any;
-    docType: any;
+  actionType: any;
+  dialogData: any;
+  actionsDataColumns: any;
+  actionsData: any;
+  createRequestForm: any;
+  file: File;
+  userInfo: any;
+  docType: any;
+  files: Array<File> = [];
 
-    constructor(@Inject(MAT_DIALOG_DATA) public data: any,
-        private load_data: LoadUserDataComponent,
-        private fb: FormBuilder,
-        private userService: UserService,
-        public dialogRef: MatDialogRef<ARequestActionComponent>,
-        private bkd: BackendUrlComponent,
-        public sharedvar: SharedVariablesComponent,) {
-        this.actionType = data.action;
-        this.dialogData = data.ComponentData;
-        this.userInfo = this.dialogData.userInfo;
-        this.docType = this.dialogData.data;
-        this.createRequestForm = FormGroup;
-        console.log("I'm dialog data : ",this.docType);
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private load_data: LoadUserDataComponent,
+    private fb: FormBuilder,
+    private userService: UserService,
+    public dialogRef: MatDialogRef<ARequestActionComponent>,
+    private bkd: BackendUrlComponent,
+    public sharedvar: SharedVariablesComponent
+  ) {
+    this.actionType = data.action;
+    this.dialogData = data.ComponentData;
+    this.userInfo = this.dialogData.userInfo;
+    this.docType = this.dialogData.data;
+    this.createRequestForm = FormGroup;
+    console.log("I'm dialog data : ", this.docType);
+  }
+
+  ngOnInit() {
+    this.createRequestForm = this.fb.group({
+      prn: new FormControl('', [Validators.required]),
+      documentType: new FormControl('', [Validators.required]),
+      approvalFrom: new FormControl('', [Validators.required]),
+      comments: new FormControl('', [Validators.required]),
+      file: new FormControl('', [Validators.required]),
+    });
+  }
+  onChange(event: any) {
+    this.files = <Array<File>>event.target.files;
+    // this.file = event.target.files[0];
+  }
+
+  getUrl(): string {
+    return this.sharedvar.backend_url + '/requests';
+  }
+
+  OnEdit() {
+    console.log(this.file);
+    const formData: any = new FormData();
+    const files: Array<File> = this.files;
+    console.log(files);
+    for (let i = 0; i < files.length; i++) {
+      formData.append('files', files[i], files[i]['name']);
     }
+    formData.append('user', this.userInfo._id);
+    formData.append('document_type', this.docType._id);
+    formData.append('requiredValidations', 'scholarship');
+    formData.append('comments', this.createRequestForm.comment);
+    console.log(this.createRequestForm.comments);
 
-    ngOnInit() {
-        this.createRequestForm = this.fb.group({
-            prn: new FormControl('', [Validators.required]),
-            documentType: new FormControl('', [Validators.required]),
-            approvalFrom :new FormControl('', [Validators.required]),
-            comment:new FormControl('', [Validators.required]),
-            file : new FormControl('', [Validators.required])
-          })
-
-          
-    }
-    onChange(event: any) {
-        this.file = event.target.files[0];
-    }
-
-    getUrl(): string {
-        return (this.sharedvar.backend_url + '/requests');
-    }
-
-    OnEdit() {
-        console.log(this.file)
-        const sendDataToBackend = {
-            "user": this.userInfo._id,
-            "document_type": this.docType._id,
-            "requiredValidations" : "scholarship",
-            "files":this.file,
-            "comments":this.createRequestForm.comment
-
+    this.userService.addRequest(this.getUrl(), formData).subscribe(
+      (data) => {
+        if (data) {
+          this.dialogRef.close();
+        } else {
         }
-        console.log("send",sendDataToBackend);
-        this.userService.addRequest(this.getUrl(), sendDataToBackend).subscribe(
-            data => {
-                if (data) {
-                    this.dialogRef.close();
-                }
-                else {
+        // console.log(data);
+      },
+      (err) => {}
+    );
+  }
 
-                }
-                // console.log(data);
-            },
-            err => {
-
-            }
-        );
-    }
-
-    OnCancel() {
-        this.dialogRef.close();
-    }
+  OnCancel() {
+    this.dialogRef.close();
+  }
 }
